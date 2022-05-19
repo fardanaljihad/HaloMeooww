dc0696e05c0a437879ce05e9e8f20210224c3b40
fardantif21 <fardan.al.tif421@polban.ac.id>
2022-05-19 14:00:06 +0700

    Update queue.h

--- list.h
+++ list.h
@@ -1,4 +1,20 @@
 #ifndef __LIST_H__
 #define __LIST_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct tInfoPenyakit {
+	char namaPenyakit[30];
+	char kategori[10];
+};
+typedef struct tNodeList *addressNodeList;
+typedef struct tNodeList {
+	int indeks;
+	addressNodeList next;
+} NodeList;
+typedef struct {
+	addressNodeList First;
+} List;
+
 #endif
--- queue.h
+++ queue.h
@@ -4,7 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "list.h"
 
+typedef struct tNodeQueue *addressNodeQueue;
+typedef struct tNodeQueue {
+	char namaPemilik[20];
+	char namaHewan[20];
+	List daftarPenyakit;
+	int nilaiPrioritas;
+	int waktuKedatangan;
+	int waktuMulai;
+	int waktuPelayanan;
+	int waktuSelesai;
+} NodeQueue;
+typedef struct {
+	addressNodeQueue Front;
+	addressNodeQueue Rear;
+} Queue;
 
+void CreateQueue(Queue *Q);
 
 #endif
